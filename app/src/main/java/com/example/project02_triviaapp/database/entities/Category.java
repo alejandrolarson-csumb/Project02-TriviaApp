4efb801ad0a69a65baff082e192db4a83ef82980
AlejandroLarson <18639325+AlejandroLarson@users.noreply.github.com>
2024-12-10 07:39:43 -0800

    added Category.java

---
+++ app/src/main/java/com/example/project02_triviaapp/database/entities/Category.java
@@ -0,0 +1,48 @@
+package com.example.project02_triviaapp.database.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+@Entity(tableName = "category_table")
+public class Category {
+    @PrimaryKey(autoGenerate = true)
+    private int categoryId;
+    private String categoryText;
+
+    public Category(@NonNull String category) {
+        this.categoryText = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return categoryId == category.categoryId && Objects.equals(categoryText, category.categoryText);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryId, categoryText);
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryText() {
+        return categoryText;
+    }
+
+    public void setCategoryText(String categoryText) {
+        this.categoryText = categoryText;
+    }
+}
