576cf3152b5f6119a48cf870c3069cfd402b1c71
SRitter1382 <sritter@csumb.edu>
2024-12-10 21:07:11 -0800

    Created alertDialog for logout menu. User will stay logged in if account closes.

--- app/src/main/java/com/example/project02_triviaapp/MainActivity.java
+++ app/src/main/java/com/example/project02_triviaapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.project02_triviaapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,7 +13,9 @@ import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
 import com.example.project02_triviaapp.database.TriviaRepository;
 import com.example.project02_triviaapp.database.entities.User;
@@ -20,12 +24,17 @@ import com.example.project02_triviaapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private static final String MAIN_ACTIVITY_USER_ID = "com.example.project02_triviaapp.MAIN_ACTIVITY_USER_ID";
+    static final String SHARED_PREFERENCE_USERID_KEY = "com.example.project02_triviaapp.SHARED_PREFERENCE_USERID_KEY";
+    static final String SAVED_INSTANCE_STATE_USERID_KEY = "com.example.project02_triviaapp.SAVED_INSTANCE_STATE_USERID_KEY";
+
     public static final String TAG = "DAC_TRIVIALOG";
-    private static final int LOGGED_OUT = -1;
     private ActivityMainBinding binding;
     private TriviaRepository repository;
     private int loggedInUserId = -1;
+    private int LOGGED_OUT = -1;
     private User user;
+
+    //TODO: If logged in user is Admin, set visibility of Admin button to visible
 
 
     @Override
@@ -37,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
         invalidateOptionsMenu();
 
         repository = TriviaRepository.getRepository(getApplication());
+        loginUser(savedInstanceState);
+
+        if (loggedInUserId == -1) {
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+        updateSharedPreference();
 
         binding.isAdminButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,15 +81,85 @@ public class MainActivity extends AppCompatActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         MenuItem item = menu.findItem(R.id.logout_menu);
         item.setVisible(true);
+        //TODO:Figure out why line below is appearing as null
         //item.setTitle(user.getUsername());
         item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(@NonNull MenuItem item) {
                 Toast.makeText(MainActivity.this, "Logout to be implemented", Toast.LENGTH_SHORT).show();
-                //logout()
+                showLogoutDialog();
                 return false;
             }
         });
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        alertBuilder.setMessage("Logout?");
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+        alertBuilder.create().show();
+    }
+
+    private void loginUser(Bundle savedInstanceState){
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        loggedInUserId = sharedPreferences.getInt(getString(R.string.preference_userid_key), LOGGED_OUT);
+
+        if (loggedInUserId == LOGGED_OUT & savedInstanceState != null) {
+            loggedInUserId = savedInstanceState.getInt(SHARED_PREFERENCE_USERID_KEY, LOGGED_OUT);
+        }
+        if (loggedInUserId == LOGGED_OUT) {
+            loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        }
+        if (loggedInUserId == LOGGED_OUT) {
+            return;
+        }
+        LiveData<User> userObserver = repository.getUserByUserId(loggedInUserId);
+        userObserver.observe(this, user -> {
+            this.user = user;
+            if (this.user != null) {
+                invalidateOptionsMenu();
+            }
+        });
+    }
+
+    private void logout() {
+        loggedInUserId = LOGGED_OUT;
+        updateSharedPreference();
+
+        getIntent().putExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+    }
+
+
+    private void updateSharedPreference() {
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+        sharedPrefEditor.putInt(getString(R.string.preference_userid_key), loggedInUserId);
+        sharedPrefEditor.apply();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVED_INSTANCE_STATE_USERID_KEY, loggedInUserId);
+        updateSharedPreference();
     }
 }
