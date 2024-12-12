f9fa4fed94a3ce0c7e15f5e9f520b9e4e67d84c7
benshimek <bshimek@csumb.edu>
2024-12-12 12:21:13 -0800

    Updated comments and added Javadoc

--- app/src/main/java/com/example/project02_triviaapp/CategoryHighScoresActivity.java
+++ app/src/main/java/com/example/project02_triviaapp/CategoryHighScoresActivity.java
@@ -6,12 +6,29 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.project02_triviaapp.database.entities.Category;
 
+/**
+ * @author Ben Shimek
+ * CategoryHighScoresActivity displays a set of buttons representing different trivia categories.
+ * When a user clicks on a category, the app will navigate to the leaderboard screen for that category.
+ *
+ * The activity is designed to let users select categories such as Movies, History, and Other Categories,
+ * and then view the top scores for the selected category by navigating to the LeaderboardActivity.
+ */
+
 public class CategoryHighScoresActivity extends AppCompatActivity {
 
     private Button moviesButton;
     private Button historyButton;
     private Button otherCategoryButton;
     private Button otherCategoryButton2;
+
+    /**
+     * @author Ben Shimek
+     * Called when the activity is created.
+     * This method initializes the buttons, sets up onClick listeners, and starts the appropriate activity
+     * when a category is selected by the user.
+     * @param savedInstanceState The saved instance state of the activity.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +42,19 @@ public class CategoryHighScoresActivity extends AppCompatActivity {
         otherCategoryButton2 = findViewById(R.id.other_category_button2);
 
         // Set onClickListeners for each button
-        moviesButton.setOnClickListener(v -> openLeaderboard(Category.MOVIES));
+        moviesButton.setOnClickListener(v -> openLeaderboard(Category.MOVIES)); // TODO: need to create constant fields in Category.java
         historyButton.setOnClickListener(v -> openLeaderboard(Category.HISTORY));
         otherCategoryButton.setOnClickListener(v -> openLeaderboard(Category.OTHER_CATEGORY));
         otherCategoryButton2.setOnClickListener(v -> openLeaderboard(Category.OTHER_CATEGORY_2));
 
     }
 
-    private void openLeaderboard(String category) {
+    /**
+     * @author Ben Shimek
+     * Starts the LeaderboardActivity and passes the selected category to it via an Intent.
+     * @param category The category whose leaderboard is to be displayed. This value will be passed to LeaderboardActivity.
+     */
+    private void openLeaderboard(String category) {  //TODO: need to create LeaderboardActivity.java
         Intent intent = new Intent(CategoryHighScoresActivity.this, LeaderboardActivity.class);
         intent.putExtra("category", category);
         startActivity(intent);
