773195869b24a0fab4c5c0c78b9089e981f64c03
benshimek <bshimek@csumb.edu>
2024-12-12 13:01:10 -0800

    Created LeaderboardActivity.java

---
+++ app/src/main/java/com/example/project02_triviaapp/LeaderboardActivity.java
@@ -0,0 +1,28 @@
+package com.example.project02_triviaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    private TextView leaderboardText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard); //TODO: need to make activity_leaderboard.xml
+
+        leaderboardText = findViewById(R.id.leaderboard_text);
+
+        // Get the category from the intent
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
+
+        // Get the top 3 scores for the selected category
+        getTopScores(category); //TODO: need to make getTopScores() method
+    }
+
+}
+
