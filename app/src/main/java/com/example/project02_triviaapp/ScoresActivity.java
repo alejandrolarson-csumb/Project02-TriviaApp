8e0116df755e5496203ca5fb929cea2e2f55cd20
benshimek <bshimek@csumb.edu>
2024-12-11 21:20:24 -0800

    Created ScoresActivity.java Initialized views for activity_scores.xml Created intent to get data passed from GameplayActivity Displayed the Final Score Called saveScoreToDatabase(), still need to create method

---
+++ app/src/main/java/com/example/project02_triviaapp/ScoresActivity.java
@@ -0,0 +1,39 @@
+package com.example.project02_triviaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoresActivity extends AppCompatActivity {
+
+    private TextView finalScoreText;
+    private Button returnToMainMenu;
+
+    // variables to be passed from GameplayActivity
+    private int finalScore;
+    private long userId;
+    private long categoryId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+
+        finalScoreText = findViewById(R.id.final_score_text);
+        submitScoreButton = findViewById(R.id.submit_score_button);
+
+        // Get the data passed from GameActivity
+        Intent intent = getIntent();
+        // Will be passed form GameActivity to ScoresActivity
+        finalScore = intent.getIntExtra("final_score", 0);
+        userId = intent.getLongExtra("user_id", 0);
+        categoryId = intent.getLongExtra("category_id", 0);
+
+        finalScoreText.setText("Final Score: " + finalScore); // Display the final score
+
+        saveScoreToDatabase();  // save the score
+    }
+
+}
