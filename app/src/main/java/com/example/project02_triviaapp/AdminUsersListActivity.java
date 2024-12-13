a44d212a997c393be3293d5eb018e3005c73629a
AlejandroLarson <18639325+AlejandroLarson@users.noreply.github.com>
2024-12-12 23:10:52 -0800

    Set onClick event listener to back button and used intent factory to return to admin panel

--- app/src/main/java/com/example/project02_triviaapp/AdminUsersListActivity.java
+++ app/src/main/java/com/example/project02_triviaapp/AdminUsersListActivity.java
@@ -3,6 +3,7 @@ package com.example.project02_triviaapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,13 @@ public class AdminUsersListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityAdminUsersListBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.adminUserListBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminPanelActivity.AdminPanelIntentFactory(getApplicationContext())));
+            }
+        });
 
     }
 
